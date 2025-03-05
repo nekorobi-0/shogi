c70bc630c47b43fa006e61a0fbfaaaa212a2095a
taaakuma <takuma20070421@gmail.com>
2025-03-05 21:01:23 +0900

    当たり判定の保管の実装

--- canvas.js
+++ canvas.js
@@ -15,6 +15,15 @@ window.addEventListener("resize", update_canvas);
  * 都合上、子スプライトが描画されると、親スプライトも描画されるようにする
  */
 let canvas = {
+    /**
+     * 当たり判定
+     * [sprite,x,y,width,height]
+     * 0に近いほど優先
+     * 
+     * 補足:描画される順番はspritesの順通りとは限らないから、配列spritesでは当たり判定がうまくいかない場合がある
+     */
+    hitbox: [],
+    _hitbox: [], //描画が完了次第hitboxに反映させる。 
     dom: document.getElementsByTagName("canvas")[0],
     ctx: document.getElementsByTagName("canvas")[0].getContext("2d"),
     //sprites: 表示する順にidを配置する インデックスが小さい方が先に表示される。つまり背面に配置される。
@@ -45,6 +54,12 @@ let canvas = {
 
         canvas.ctx.drawImage(sprite.offscreen, sprite.x, sprite.y,sprite.width,sprite.height);
 
+        
+        //当たり判定の記述
+        if(sprite.hitbox) {
+            this._hitbox.push({"sprite": sprite,"x": sprite.x,"y": sprite.y,"width": sprite.width,"height": sprite.height});
+        }
+        
 
     },
     frame: function(){
@@ -57,6 +72,8 @@ let canvas = {
             let _sprite = canvas.sprites_list[canvas.sprites[i]];
             canvas.drawing(_sprite);
         }
+        this.hitbox = this._hitbox;
+        this._hitbox = [];
         requestAnimationFrame(canvas.frame);
     }
 }
