ea60f6ffb3116640d63b819982b845cfb6ce4ff6
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2025-03-05 20:47:47 +0900

    将棋駒クラス(仮)

--- shogi.js
+++ shogi.js
@@ -1,6 +1,23 @@
-class ShogiPiece extends Sprite {
+class ShogiPiece {
     constructor(x, y, type) {
-        super(x, y, 100, 100);
+        this.backgtound = new text();
+        this.backgtound.text = "☗";
+        this.backgtound.width = 150
+        this.backgtound.height = 150
+        this.backgtound.fontColor = "#DACA9E";
+
+        this.letter = new text();
+        this.letter.text = type;
+        this.letter.width = 80
+        this.letter.height = 80
+        this.letter.parent = this.backgtound;
+
+        this.letter.relativeX = 35;
+        this.letter.relativeY = 35;
+        this.letter.fontColor = "#000000";
+
+        this.text.text = type;
+        this.text
         this.piece = type;
     }
 }
