12f8a237caba94ff5092779b0cca089e8d4f3e2a
taaakuma <takuma20070421@gmail.com>
2025-03-06 01:51:32 +0900

    ネズミ操作対応、選択された駒の色が変わるように

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-let player = 1;
+selectX = null;
+selectY = null
+
+stage.event.click = function(e){
+
+    let x = e.gridX;
+    let y = e.gridY;
+
+    if(selectX == null || selectY == null){
+        selectX = x;
+        selectY = y;
+        manager.board[y][x].background.fontColor = 'orange'
+    }else{
+        manager.board[selectY][selectX].background.fontColor = '#DACA9E'
+        let flag = manager.move(selectY,selectX,y,x)
+        console.log(selectY,selectX,y,x)
+        
+        selectX = null;
+        selectY = null;
+        
+    }
+    console.log(x,y);
+}
